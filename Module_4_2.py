be7aed54089dd3e3c02607f1a009c604969f70db
SSasha88 <d1rus1853@gmail.com>
2024-12-12 15:52:49 +0300

    + Домашняя работа по уроку "Модули и пакеты", выполнено

---
+++ Module_4_2.py
@@ -0,0 +1,15 @@
+
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    # Вызываю внутреннюю функцию внутри внешней функции
+    inner_function()
+
+
+# Вызываею внешнюю функцию
+test_function()
+
+# Пробую вызвать внутреннюю функцию снаружи
+inner_function()
